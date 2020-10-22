85cb7c07e8942d23a4d9484c34d45c5a3ce27c1e
bambambigalo <kri69rus2016@yandex.ru>
2020-10-22 11:02:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-propsi-hududu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-propsi-hududu)
---
+++ src/pages/Apple.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Apple () {
+  return (
+    <div>
+    Apple
+    <div className = 'apple' >
+    
+    </div>
+    </div>
+  )
+
+  
+}
